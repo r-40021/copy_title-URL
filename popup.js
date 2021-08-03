c66f71da1726c435b6d223f23521083e5e744c8a
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-08-03 14:57:09 +0900

    [Add]JavaScript

--- popup.js
+++ popup.js
@@ -1,3 +1,7 @@
-setTimeout(() => {
-    alert("Hello, world! (from js)");
-}, 700);
+const title = document.getElementById("pageTitle");
+const url = document.getElementById("pageURL");
+
+chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, tabs => {
+    title.value = tabs[0].title;
+    url.value = tabs[0].url;
+});
