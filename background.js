0d04c1f7bbc8a237466a9d949f250d32ae70928b
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-08-31 21:25:00 +0900

    [Add]English

--- background.js
+++ background.js
@@ -1,54 +1,103 @@
 chrome.runtime.onInstalled.addListener(function (details) {
-    /* コンテキストメニューを作成 */
-    const parent = chrome.contextMenus.create({
-      id: "share",
-      title: "ページを共有",
-      contexts: ["all"],
-    });
+  /* コンテキストメニューを作成 */
+  const menuText = {
+    parent: { ja: "ページを共有", en: "Share this page" },
+    title: { ja: "ページタイトルをコピー", en: "Copy title" },
+    url: { ja: "URL をコピー", en: "Copy URL" },
+    both: { ja: "ページタイトルと URL をコピー", en: "Copy both title and URL" },
+    fb: { ja: "Facebook でシェア", en: "Share on Facebook" },
+    tweet: { ja: "ツイート", en: "Tweet" },
+    li: { ja: "LINE で送る", en: "Share on LinkedIn" },
+    mail: { ja: "メールで送信", en: "Email" }
+  }
+  const parent = chrome.contextMenus.create({
+    id: "share",
+    title: getText("parent"),
+    contexts: ["all"],
+  });
 
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "title",
-      title: "ページタイトルをコピー",
-      contexts: ["all"],
-    });
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "URL",
-      title: "URL をコピー",
-      contexts: ["all"],
-    });
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "both",
-      title: "ページタイトルと URL をコピー",
-      contexts: ["all"],
-    });
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "FB",
-      title: "Facebook でシェア",
-      contexts: ["all"],
-    });
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "tweet",
-      title: "ツイート",
-      contexts: ["all"],
-    });
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "LINE",
-      title: "LINE で送る",
-      contexts: ["all"],
-    });
-    chrome.contextMenus.create({
-      parentId: parent,
-      id: "mail",
-      title: "メールで送信",
-      contexts: ["all"],
-    });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "title",
+    title: getText("title"),
+    contexts: ["all"],
+  });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "URL",
+    title: getText("url"),
+    contexts: ["all"],
+  });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "both",
+    title: getText("both"),
+    contexts: ["all"],
+  });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "FB",
+    title: getText("fb"),
+    contexts: ["all"],
+  });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "tweet",
+    title: getText("tweet"),
+    contexts: ["all"],
+  });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "li",
+    title: getText("li"),
+    contexts: ["all"],
+  });
+  chrome.contextMenus.create({
+    parentId: parent,
+    id: "mail",
+    title: getText("mail"),
+    contexts: ["all"],
+  });
+
+  function getText(type) {
+    switch (type) {
+      case "parent":
+        return lang(menuText.parent);
+
+      case "title":
+        return lang(menuText.title);
+
+      case "url":
+        return lang(menuText.url);
+
+      case "both":
+        return lang(menuText.both);
+
+      case "fb":
+        return lang(menuText.fb);
+
+      case "tweet":
+        return lang(menuText.tweet);
+
+      case "li":
+        return lang(menuText.li);
+
+      case "mail":
+        return lang(menuText.mail);
+    }
+
+    function lang(string) {
+      switch (navigator.language) {
+        case "ja":
+          return string.ja;
+        default:
+          return string.en;
+      }
+    }
+  }
 });
+
+
 
 /* コンテキストメニューがクリックされた時の処理 */
 chrome.contextMenus.onClicked.addListener((info, tab) => {
@@ -88,10 +137,10 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       });
       break;
 
-    case "LINE":
+    case "li":
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: LINE,
+        function: li,
       });
       break;
     case "mail":
@@ -139,28 +188,29 @@ function FB() {
 function tweet() {
   window.open(
     "https://twitter.com/intent/tweet?text=" +
-      encodeURIComponent(document.title) +
-      "%0a&url=" +
-      encodeURIComponent(location.href),
+    encodeURIComponent(document.title) +
+    "%0a&url=" +
+    encodeURIComponent(location.href),
     "tweetwindow",
     "width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1"
   );
 }
-function LINE() {
+function li() {
   window.open(
-    "https://social-plugins.line.me/lineit/share?url=" +
-      encodeURIComponent(location.href),
+    (navigator.language === "ja" ? "https://social-plugins.line.me/lineit/share?url=" : "https://www.linkedin.com/sharing/share-offsite/?url=") +
+    encodeURIComponent(location.href),
     "tweetwindow",
     "width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1"
   );
 }
-function mail(){
+
+function mail() {
   window.open(
-    "mailto:?Subject=" + encodeURIComponent("【Web ページの共有】" +
-    document.title) + 
+    "mailto:?Subject=" + encodeURIComponent((navigator.language === "ja" ? "【Web ページの共有】" : "Shared page: ") +
+      document.title) +
     "&body=" +
     encodeURIComponent(document.title) + "%0D%0A" +
-      encodeURIComponent(location.href),
+    encodeURIComponent(location.href),
     "tweetwindow",
     "width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1"
   );
--- popup.js
+++ popup.js
@@ -1,3 +1,26 @@
+const lang = chrome.i18n.getMessage("lang");
+if (lang === "ja") {
+    document.getElementById("linkWrapper").remove();
+    document.getElementById("LINEWrapper").style.display = "";
+} else {
+    document.getElementById("LINEWrapper").remove();
+}
+const global = document.getElementsByClassName("global");
+for (let i = 0; i < global.length; i++) {
+    const element = global[i];
+    element.textContent = chrome.i18n.getMessage(element.getAttribute("msg"));
+}
+
+const globalTitle = document.getElementsByClassName("globalTitle");
+for (let i = 0; i < globalTitle.length; i++) {
+    const element = globalTitle[i];
+    const message = chrome.i18n.getMessage(element.getAttribute("msg"));
+    element.setAttribute("title", message);
+    if (element.classList.contains("label")) {
+        element.setAttribute("aria-label", message);
+    }
+}
+
 const title = document.getElementById("pageTitle");
 const url = document.getElementById("pageURL");
 
@@ -7,7 +30,7 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, tabs => {
 
     title.value = tabTitle;
     url.value = tabUrl;
-    
+
     document.getElementById("copyTitle").addEventListener("click", () => {
         navigator.clipboard.writeText(tabTitle);
         showCheck("copyTitle");
@@ -28,17 +51,23 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, tabs => {
     document.getElementById("tweet").addEventListener("click", () => {
         windowOpen("https://twitter.com/intent/tweet?text=" + encodeURIComponent(tabTitle) + "%0a&url=" + encodeURIComponent(tabUrl));
     }, false);
-    document.getElementById("LINE").addEventListener("click", ()=>{
-        windowOpen("https://social-plugins.line.me/lineit/share?url=" + encodeURIComponent(tabUrl));
+    if (lang === "ja") {
+        document.getElementById("LINE").addEventListener("click", () => {
+            windowOpen("https://social-plugins.line.me/lineit/share?url=" + encodeURIComponent(tabUrl));
+        }, false);
+    } else {
+        document.getElementById("link").addEventListener("click", () => {
+            windowOpen("https://www.linkedin.com/sharing/share-offsite/?url=" + encodeURIComponent(tabUrl));
+        }, false);
+    }
+    document.getElementById("mail").addEventListener("click", () => {
+        windowOpen("mailto:?Subject=" + encodeURIComponent(chrome.i18n.getMessage("mailBody") + tabTitle) + "&body=" + encodeURIComponent(tabTitle) + "%0D%0A" + encodeURIComponent(tabUrl));
     }, false);
-     document.getElementById("mail").addEventListener("click", ()=>{
-        windowOpen("mailto:?Subject=" + encodeURIComponent("【Web ページの共有】" + tabTitle) + "&body=" + encodeURIComponent(tabTitle) + "%0D%0A" + encodeURIComponent(tabUrl));
-    }, false);
-    
-    if(!/http\:\/\/|https\:\/\//.test(tabUrl)) {
+
+    if (!/http\:\/\/|https\:\/\//.test(tabUrl)) {
         const element = document.getElementById("sns");
         element.style.marginTop = "1.5em";
-        element.textContent = "このページでは SNS を用いたシェア機能をご利用になれません。ご了承ください。";
+        element.textContent = chrome.i18n.getMessage("alert");
     }
 
 });
@@ -49,19 +78,24 @@ function showCheck(parentId) {
     const checkIcons = document.getElementsByClassName("checkIcon");
     for (let i = 0; i < checkIcons.length; i++) {
         checkIcons[i].style.display = "none";
+        checkIcons[i].setAttribute("aria-hidden", true);
     }
     const clipIcons = document.getElementsByClassName("bi-clipboard");
     for (let i = 0; i < clipIcons.length; i++) {
         clipIcons[i].style.display = "";
-
+        checkIcons[i].setAttribute("aria-hidden", false);
     }
     const child = document.getElementById(parentId).children;
     child[0].style.display = "none";
+    child[0].setAttribute("aria-hidden", true);
     child[1].style.display = "";
+    child[1].setAttribute("aria-hidden", false);
 
     timeout = setTimeout(() => {
         child[0].style.display = "";
+        child[0].setAttribute("aria-hidden", false);
         child[1].style.display = "none";
+        child[1].setAttribute("aria-hidden", true);
     }, 5000);
 }
 
